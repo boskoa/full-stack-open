922d658d54b75c45e21b4aa89cad4dc6774457fb
boskoa <bosko.ap@gmail.com>
2022-04-04 23:22:40 +0200

    Adding exercises 9.19.-9.21.

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -4,9 +4,44 @@ export interface Diagnose {
   latin?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+export interface Discharge {
+  date: string;
+  criteria: string;
 }
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -28,4 +63,11 @@ export enum Gender {
   Male = 'male',
   Female = 'female',
   Other = 'other'
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
 }
